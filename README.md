2a177966d2a7c45fb0e76c9bf2a349b3dd5bcc10
JulienPeloton <peloton@lal.in2p3.fr>
2023-08-09 10:31:15 +0200

    New dataloader

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 This repository contains:
 - [fink_spins](fink_spins), a library with necessary routines to work with spin orientation of asteroids
-- [data](data/) for various absolute magnitude models
+- [data](data/) for various absolute magnitude models + instruction to generate data for notebooks
 - [notebooks](notebooks) describing different models, and with various examples and validation steps
--- fink_spins/generate_data.py
+++ fink_spins/generate_data.py
@@ -1,5 +1,5 @@
 # Copyright 2023 AstroLab Software
-# Author: Roman Le Montagner, Benoit Carry
+# Author: Roman Le Montagner, Benoit Carry, Julien Peloton
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,297 +12,163 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+""" Script to generate aggregated data from the SSOFT """
+import requests
+import argparse
+import logging
+import io
+import sys
 
 import numpy as np
 import pandas as pd
-import requests
-import io
 
 import rocks
-import sys
-
 
 rocks.set_log_level("critical")
 
 
-def format_HG(data_hg: pd.DataFrame, filters: dict) -> pd.DataFrame:
+def format_ssoft(data: pd.DataFrame, filters: dict, flavor: str) -> pd.DataFrame:
+    """ Formatting of the SSOFT columns
     """
-    Format the columns of the HG dataframe
+    shared_cols = [
+        'min_phase',
+        'min_phase_1',
+        'min_phase_2',
+        'max_phase',
+        'max_phase_1',
+        'max_phase_2',
+        'n_obs',
+        'n_obs_1',
+        'n_obs_2',
+        'n_days',
+        'n_days_1',
+        'n_days_2',
+        'mean_astrometry',
+        'std_astrometry',
+        'skew_astrometry',
+        'kurt_astrometry',
+        'last_jd',
+        'version',
+        'sso_name',
+        'sso_number'
+    ]
+    if flavor in ['HG1G2', 'HG']:
+        # Remove columns that are shared across all SSOFT
+        data = data.drop(columns=shared_cols, errors='ignore')
 
-    Parameters
-    ----------
-    data_hg : DataFrame
-        HG data
-    filters : dict
-        filter dict
-
-    Returns
-    -------
-    pd.DataFrame
-        same as input but with columns rename and new columns to match the final dataframe of the script.
-
-    Examples
-    --------
-    >>> HG_data = format_HG(data_hg, filters)
-    >>> len(HG_data)
-    115148
-    >>> HG_data.columns
-    Index(['ssnamenr', 'nmeasurements', 'ndays', 'params', 'number', 'name',
-           'HG_H_g', 'HG_dH_g', 'HG_G_g', 'HG_dG_g', 'HG_rms_g', 'HG_H_r',
-           'HG_dH_r', 'HG_G_r', 'HG_dG_r', 'HG_rms_r', 'minphase', 'maxphase',
-           'n_days', 'n_obs', 'HG_rms', 'HG_chi2red', 'HG1G2_status', 'HG1G2_fit'],
-          dtype='object')
-    """
-
-    # HG to data
-    names_numbers = rocks.identify(data_hg.ssnamenr)
-    data_hg["number"] = [nn[1] for nn in names_numbers]
-    data_hg["name"] = [nn[0] for nn in names_numbers]
-
-    for filt in filters.keys():
-        data_hg["HG_H_{}".format(filters[filt])] = data_hg["params"].str[
-            "H_{}".format(filt)
-        ]
-        data_hg["HG_dH_{}".format(filters[filt])] = data_hg["params"].str[
-            "errH_{}".format(filt)
-        ]
-        data_hg["HG_G_{}".format(filters[filt])] = data_hg["params"].str[
-            "G_{}".format(filt)
-        ]
-        data_hg["HG_dG_{}".format(filters[filt])] = data_hg["params"].str[
-            "errG_{}".format(filt)
-        ]
-        data_hg["HG_rms_{}".format(filters[filt])] = data_hg["params"].str[
-            "rms_{}".format(filt)
-        ]
-
-    data_hg["minphase"] = np.rad2deg(data_hg["params"].str["minphase"])
-    data_hg["maxphase"] = np.rad2deg(data_hg["params"].str["maxphase"])
-
-    data_hg["n_days"] = data_hg["ndays"]
-    data_hg["n_obs"] = data_hg["nmeasurements"]
-
-    data_hg["HG_rms"] = data_hg["params"].str["rms"]
-    data_hg["HG_chi2red"] = data_hg["params"].str["chi2red"]
-
-    data_hg["HG_status"] = data_hg["params"].str["status"]
-    data_hg["HG_fit"] = data_hg["params"].str["fit"]
-
-    cond = data_hg.name.notna()
-    data_hg = data_hg[cond].reset_index(drop=True)
-
-    return data_hg
-
-
-def format_HG1G2(data_hg1g2: pd.DataFrame, filters: dict) -> pd.DataFrame:
-    """
-    Format the columns of the HG1G2 dataframe
-
-    Parameters
-    ----------
-    data_hg1g2 : DataFrame
-        HG1G2 data
-    filters : dict
-        filter dict
-
-    Returns
-    -------
-    pd.DataFrame
-        same as input but with columns rename and new columns to match the final dataframe of the script.
-
-    Examples
-    --------
-    >>> HG1G2_data = format_HG1G2(data_hg1g2, filters)
-    >>> len(HG1G2_data)
-    115238
-    >>> HG1G2_data.columns
-    Index(['ssnamenr', 'HG1G2_H_g', 'HG1G2_dH_g', 'HG1G2_G1_g', 'HG1G2_dG1_g',
-           'HG1G2_G2_g', 'HG1G2_dG2_g', 'HG1G2_rms_g', 'HG1G2_H_r', 'HG1G2_dH_r',
-           'HG1G2_G1_r', 'HG1G2_dG1_r', 'HG1G2_G2_r', 'HG1G2_dG2_r', 'HG1G2_rms_r',
-           'HG1G2_rms', 'HG1G2_chi2red', 'HG1G2_status', 'HG1G2_fit'],
-          dtype='object')
-    """
-
-    # HG1G2 to data
-    for filt in filters.keys():
-        data_hg1g2["HG1G2_H_{}".format(filters[filt])] = data_hg1g2["params"].str[
-            "H_{}".format(filt)
-        ]
-        data_hg1g2["HG1G2_dH_{}".format(filters[filt])] = data_hg1g2["params"].str[
-            "errH_{}".format(filt)
-        ]
-        data_hg1g2["HG1G2_G1_{}".format(filters[filt])] = data_hg1g2["params"].str[
-            "G1_{}".format(filt)
-        ]
-        data_hg1g2["HG1G2_dG1_{}".format(filters[filt])] = data_hg1g2["params"].str[
-            "errG1_{}".format(filt)
-        ]
-        data_hg1g2["HG1G2_G2_{}".format(filters[filt])] = data_hg1g2["params"].str[
-            "G2_{}".format(filt)
-        ]
-        data_hg1g2["HG1G2_dG2_{}".format(filters[filt])] = data_hg1g2["params"].str[
-            "errG2_{}".format(filt)
-        ]
-        data_hg1g2["HG1G2_rms_{}".format(filters[filt])] = data_hg1g2["params"].str[
-            "rms_{}".format(filt)
-        ]
-
-    data_hg1g2["HG1G2_rms"] = data_hg1g2["params"].str["rms"]
-    data_hg1g2["HG1G2_chi2red"] = data_hg1g2["params"].str["chi2red"]
-    data_hg1g2["HG1G2_status"] = data_hg1g2["params"].str["status"]
-    data_hg1g2["HG1G2_fit"] = data_hg1g2["params"].str["fit"]
-
-    data_hg1g2 = data_hg1g2.drop(columns=["nmeasurements", "ndays", "params"])
-
-    return data_hg1g2
-
-
-def format_sHG1G2(data_shg1g2: pd.DataFrame, filters: dict) -> pd.DataFrame:
-    """
-    Format the columns of the sHG1G2 dataframe
-
-    Parameters
-    ----------
-    data_shg1g2 : DataFrame
-        sHG1G2 data
-    filters : dict
-        filter dict
-
-    Returns
-    -------
-    pd.DataFrame
-        same as input but with columns rename and new columns to match the final dataframe of the script.
-
-    Examples
-    --------
-    >>> sHG1G2_data = format_sHG1G2(data_shg1g2, filters)
-    >>> len(sHG1G2_data)
-    115238
-    >>> sHG1G2_data.columns
-    Index(['ssnamenr', 'sHG1G2_H_g', 'sHG1G2_dH_g', 'sHG1G2_G1_g', 'sHG1G2_dG1_g',
-           'sHG1G2_G2_g', 'sHG1G2_dG2_g', 'sHG1G2_rms_g', 'sHG1G2_H_r',
-           'sHG1G2_dH_r', 'sHG1G2_G1_r', 'sHG1G2_dG1_r', 'sHG1G2_G2_r',
-           'sHG1G2_dG2_r', 'sHG1G2_rms_r', 'sHG1G2_alpha0', 'sHG1G2_dalpha0',
-           'sHG1G2_delta0', 'sHG1G2_ddelta0', 'sHG1G2_R', 'sHG1G2_dR', 'sHG1G2_rms',
-           'sHG1G2_chi2red', 'sHG1G2_status', 'sHG1G2_fit', 'sHG1G2_minCosLambda',
-           'sHG1G2_meanCosLambda', 'sHG1G2_maxCosLambda'],
-          dtype='object')
-    """
-
-    # HG1G2hyb to data
-    for filt in filters.keys():
-        data_shg1g2["sHG1G2_H_{}".format(filters[filt])] = data_shg1g2["params"].str[
-            "H_{}".format(filt)
-        ]
-        data_shg1g2["sHG1G2_dH_{}".format(filters[filt])] = data_shg1g2["params"].str[
-            "errH_{}".format(filt)
-        ]
-        data_shg1g2["sHG1G2_G1_{}".format(filters[filt])] = data_shg1g2["params"].str[
-            "G1_{}".format(filt)
-        ]
-        data_shg1g2["sHG1G2_dG1_{}".format(filters[filt])] = data_shg1g2["params"].str[
-            "errG1_{}".format(filt)
-        ]
-        data_shg1g2["sHG1G2_G2_{}".format(filters[filt])] = data_shg1g2["params"].str[
-            "G2_{}".format(filt)
-        ]
-        data_shg1g2["sHG1G2_dG2_{}".format(filters[filt])] = data_shg1g2["params"].str[
-            "errG2_{}".format(filt)
-        ]
-        data_shg1g2["sHG1G2_rms_{}".format(filters[filt])] = data_shg1g2["params"].str[
-            "rms_{}".format(filt)
-        ]
-
-    data_shg1g2["sHG1G2_alpha0"] = np.degrees(data_shg1g2["params"].str["alpha0"])
-    data_shg1g2["sHG1G2_dalpha0"] = np.degrees(data_shg1g2["params"].str["erralpha0"])
-    data_shg1g2["sHG1G2_delta0"] = np.degrees(data_shg1g2["params"].str["delta0"])
-    data_shg1g2["sHG1G2_ddelta0"] = np.degrees(data_shg1g2["params"].str["errdelta0"])
-    data_shg1g2["sHG1G2_R"] = data_shg1g2["params"].str["R"]
-    data_shg1g2["sHG1G2_dR"] = data_shg1g2["params"].str["errR"]
-
-    data_shg1g2["sHG1G2_rms"] = data_shg1g2["params"].str["rms"]
-    data_shg1g2["sHG1G2_chi2red"] = data_shg1g2["params"].str["chi2red"]
-    data_shg1g2["sHG1G2_status"] = data_shg1g2["params"].str["status"]
-    data_shg1g2["sHG1G2_fit"] = data_shg1g2["params"].str["fit"]
-    data_shg1g2["sHG1G2_minCosLambda"] = data_shg1g2["params"].str["minCosLambda"]
-    data_shg1g2["sHG1G2_meanCosLambda"] = data_shg1g2["params"].str["meanCosLambda"]
-    data_shg1g2["sHG1G2_maxCosLambda"] = data_shg1g2["params"].str["maxCosLambda"]
-
-    data_shg1g2 = data_shg1g2.drop(columns=["nmeasurements", "ndays", "params"])
-
-    return data_shg1g2
-
-
-def format_ssoft(data_shg1g2: pd.DataFrame, filters: dict) -> pd.DataFrame:
     dict_rename = {
-        col: f"sHG1G2_{col}" for col in data_shg1g2.columns if col != "ssnamenr"
+        col: f"{flavor}_{col}" for col in data.columns if col not in (shared_cols + ['ssnamenr'])
     }
     for filt in filters.keys():
-        dict_rename[f"H_{filt}"] = f"sHG1G2_H_{filters[filt]}"
-        dict_rename[f"err_H_{filt}"] = f"sHG1G2_dH_{filters[filt]}"
-        dict_rename[f"G1_{filt}"] = f"sHG1G2_G1_{filters[filt]}"
-        dict_rename[f"G2_{filt}"] = f"sHG1G2_G2_{filters[filt]}"
-        dict_rename[f"err_G1_{filt}"] = f"sHG1G2_dG1_{filters[filt]}"
-        dict_rename[f"err_G2_{filt}"] = f"sHG1G2_dG2_{filters[filt]}"
-        dict_rename[f"rms_{filt}"] = f"sHG1G2_rms_{filters[filt]}"
-        dict_rename[f"min_phase_{filt}"] = f"sHG1G2_min_phase_{filters[filt]}"
-        dict_rename[f"max_phase_{filt}"] = f"sHG1G2_max_phase_{filters[filt]}"
-        dict_rename[f"n_obs_{filt}"] = f"sHG1G2_n_obs_{filters[filt]}"
-        dict_rename[f"n_days_{filt}"] = f"sHG1G2_n_days_{filters[filt]}"
+        dict_rename[f"H_{filt}"] = f"{flavor}_H_{filters[filt]}"
+        dict_rename[f"err_H_{filt}"] = f"{flavor}_dH_{filters[filt]}"
+        dict_rename[f"rms_{filt}"] = f"{flavor}_rms_{filters[filt]}"
+        dict_rename[f"min_phase_{filt}"] = f"min_phase_{filters[filt]}"
+        dict_rename[f"max_phase_{filt}"] = f"max_phase_{filters[filt]}"
+        dict_rename[f"n_obs_{filt}"] = f"n_obs_{filters[filt]}"
+        dict_rename[f"n_days_{filt}"] = f"n_days_{filters[filt]}"
 
-    dict_rename["alpha0"] = "sHG1G2_alpha0"
-    dict_rename["delta0"] = "sHG1G2_delta0"
-    dict_rename["err_alpha0"] = "sHG1G2_dalpha0"
-    dict_rename["err_delta0"] = "sHG1G2_ddelta0"
-    dict_rename["err_R"] = "sHG1G2_dR"
+    if flavor == 'SHG1G2':
+        for filt in filters.keys():
+            dict_rename[f"G1_{filt}"] = f"{flavor}_G1_{filters[filt]}"
+            dict_rename[f"G2_{filt}"] = f"{flavor}_G2_{filters[filt]}"
+            dict_rename[f"err_G1_{filt}"] = f"{flavor}_dG1_{filters[filt]}"
+            dict_rename[f"err_G2_{filt}"] = f"{flavor}_dG2_{filters[filt]}"
 
-    return data_shg1g2.rename(dict_rename, axis="columns")
+        dict_rename["alpha0"] = f"{flavor}_alpha0"
+        dict_rename["delta0"] = f"{flavor}_delta0"
+        dict_rename["err_alpha0"] = f"{flavor}_dalpha0"
+        dict_rename["err_delta0"] = f"{flavor}_ddelta0"
+        dict_rename["err_R"] = f"{flavor}_dR"
+    elif flavor == 'HG1G2':
+        for filt in filters.keys():
+            dict_rename[f"G1_{filt}"] = f"{flavor}_G1_{filters[filt]}"
+            dict_rename[f"G2_{filt}"] = f"{flavor}_G2_{filters[filt]}"
+            dict_rename[f"err_G1_{filt}"] = f"{flavor}_dG1_{filters[filt]}"
+            dict_rename[f"err_G2_{filt}"] = f"{flavor}_dG2_{filters[filt]}"
+    elif flavor == 'HG':
+        for filt in filters.keys():
+            dict_rename[f"G_{filt}"] = f"{flavor}_G_{filters[filt]}"
+            dict_rename[f"err_G_{filt}"] = f"{flavor}_dG_{filters[filt]}"
+
+    return data.rename(dict_rename, axis="columns")
 
 
 if __name__ == "__main__":
-    # Local Configuration
-    data_fink = "data/ztf/"
+    """
+    """
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        '-version', type=str, default='2023.08',
+        help="Version of the SSOFT. Default is 2023.08")
+    parser.add_argument(
+        '-output_dir', type=str, default='data/ztf/',
+        help="Output folder to store data. Default is data/ztf")
+    parser.add_argument(
+        '-api_url', type=str, default='https://fink-portal.org',
+        help="API URL from Fink. Default is https://fink-portal.org.")
+    parser.add_argument(
+        '--rocks', action='store_true',
+        help="If specified, override names & numbers from the SSOFT by running rocks on `ssnamenr`.")
+    parser.add_argument(
+        '--verbose', action='store_true',
+        help="If specified, print on screen information about the processing.")
+    args = parser.parse_args(None)
 
     # ZTF filters 1: g, 2: r
     filters = {"1": "g", "2": "r"}
 
-    r = requests.post(
-        sys.argv[1],
-        json={"output-format": "parquet"},
-    )
-    data_shg1g2 = pd.read_parquet(io.BytesIO(r.content))
-    # data_shg1g2 = pd.read_parquet(f"{data_fink}sso_fink_SHG1G2_sign.parquet")
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+        logging.info('Downloading the SSOFT from Fink servers...')
 
-    # data_shg1g2 = pd.read_parquet(f"{data_fink}sso_fink_SHG1G2.parquet")  # All
-    data_hg1g2 = pd.read_parquet(f"{data_fink}sso_fink_HG1G2.parquet")
-    data_hg = pd.read_parquet(f"{data_fink}sso_fink_HG.parquet")
-
-    print(
-        "check data:\n\tlen(HG): {}\n\tlen(HG1G2): {}\n\tlen(sHG1G2): {}".format(
-            len(data_hg), len(data_hg1g2), len(data_shg1g2)
+    flavors = ['SHG1G2', 'HG1G2', 'HG']
+    container = []
+    for flavor in flavors:
+        r = requests.post(
+            '{}/api/v1/ssoft'.format(args.api_url),
+            json={
+                "version": args.version,
+                "flavor": flavor,
+                "output-format": "parquet"
+            },
         )
-    )
+        container.append(pd.read_parquet(io.BytesIO(r.content)))
 
-    data_hg = format_HG(data_hg, filters)
-    data_hg1g2 = format_HG1G2(data_hg1g2, filters)
-    data_shg1g2 = format_ssoft(data_shg1g2, filters)
+    if args.verbose:
+        msg = "check data:\n\tlen({}): {}\n\tlen({}): {}\n\tlen({}): {}".format(
+            *[y for x in zip(flavors, [len(i) for i in container]) for y in x]
+        )
+        logging.info(msg)
+
+    if args.verbose:
+        logging.info('Merging the 3 SSOFTs...')
+
+    for index in range(len(container)):
+        # inplace replacement
+        container[index] = format_ssoft(container[index], filters, flavor=flavors[index])
 
     # HG with HG1G2
-    data_2 = data_hg.merge(data_hg1g2, on="ssnamenr")
-
+    tmp = container[2].merge(container[1], on="ssnamenr")
     # (HG with HG1G2) with sHG1G2
-    data = data_2.merge(data_shg1g2, on="ssnamenr")
+    data = tmp.merge(container[0], on="ssnamenr")
 
-    print("--- Output columns ---")
-    print(data.columns)
-    print("------")
-
-    print(
-        "check data:\n\tlen(HG): {}\n\tlen(HG1G2): {}\n\tlen(sHG1G2): {}\n\tlen(final data format): {}".format(
-            len(data_hg), len(data_hg1g2), len(data_shg1g2), len(data)
-        )
-    )
+    if args.rocks:
+        logging.info('Identifying names with rocks...')
+        names_numbers = rocks.identify(data.ssnamenr)
+        data["number"] = [nn[1] for nn in names_numbers]
+        data["name"] = [nn[0] for nn in names_numbers]
+    else:
+        # Names are already given in the SSOFT
+        dict_rename = {'sso_number': 'number', 'sso_name': 'name'}
+        data = data.rename(dict_rename, axis="columns")
 
     data = data.astype({"number": "Int64"})
 
-    data.to_parquet(f"{data_fink}sso_ZTF.parquet")
+    if args.verbose:
+        logging.info('Here is the first row ({} columns): '.format(len(data.columns)))
+        logging.info(data.head(1).to_dict(orient='index'))
+
+    if args.verbose:
+        logging.info('Saving to {}'.format(args.output_dir))
+    data.to_parquet("{}/sso_ZTF.parquet".format(args.output_dir))
