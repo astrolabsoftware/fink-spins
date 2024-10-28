f649a2f2fccc242fc4ad50e4c13472c448cd3255
Benoit Carry <benoit.carry@oca.eu>
2024-10-28 07:39:42 +0100

    adapted get_bft to new naming convention

--- data/get_bft.sh
+++ data/get_bft.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Get BFT
 
-wget "https://ssp.imcce.fr/data/ssoBFT-latest.parquet" -O ssoBFT-latest.parquet
+wget "https://ssp.imcce.fr/data/ssoBFT-latest_Asteroid.parquet" -O ssoBFT-latest_Asteroid.parquet
